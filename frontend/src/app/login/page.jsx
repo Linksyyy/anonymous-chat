3b9f40e9e2770ac6d4f4cefd2092b528ae10558a
Linksyyy <enzovidero@gmail.com>
2025-09-09 09:20:50 -0300

    Adds initial login page UI

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,55 @@
+"use client";
+import { useEffect, useState } from "react";
+import { CiLogin, CiLogout } from "react-icons/ci";
+
+export default function Login() {
+  const [user, setUser] = useState(null);
+  const [password, setPassword] = useState(null);
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  useEffect(() => console.log(user, password), [user, password]);
+
+  return (
+    <div className="fixed inset-0 bg-black/25 flex items-start justify-center pt-40 overflow-y-auto z-50">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-secondary w-full max-w-md rounded-2xl p-8 flex flex-col"
+      >
+        <h1 className="text-4xl p-1 justify-center flex mb-4 font-extrabold">
+          Login
+        </h1>
+        <div className="mb-4">
+          <label className="text-lg mb-1">User</label>
+          <input
+            autoFocus
+            type="text"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+            className="w-full p-2 text-lg bg-tertiary mt-1 h-10 rounded"
+          />
+        </div>
+        <div>
+          <label className="text-lg mb-1">Password</label>
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full bg-tertiary mt-1 h-10 rounded"
+          />
+        </div>
+        <div className="flex justify-between mt-10">
+          <button className="bg-zinc-600 hover:bg-zinc-700 gap-2 font-bold py-2 px-4 rounded flex items-center">
+            <CiLogout /> Cancel
+          </button>
+          <button
+            type="submit"
+            className="bg-green-600 hover:bg-green-700 gap-2 font-bold py-2 px-4 rounded flex items-center"
+          >
+            <CiLogin /> Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
