c2001d907a0377f0de0d7383fff776bcac0af696
Linksyyy <enzovidero@gmail.com>
2025-09-09 00:30:27 -0300

    Migrate frontend to Next.js framework and tailwind to postcss extension

--- frontend/src/Components/ChatsList.jsx
+++ frontend/src/Components/ChatsList.jsx
@@ -1,24 +1,15 @@
-import { Component } from "react";
+"use client";
+import { useState } from "react";
 
-export default class ChatsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chats: this.props.chats || [],
-    };
-  }
-  MessageList;
-
-  render() {
-    const { chats } = this.state;
-    return (
-      <aside className="bg-primary border-r-1 border-secondary gap-2 flex h-full absolute w-64 flex-col">
-        {chats.map((chat) => (
-          <div className="bg-secondary hover:bg-tertiary p-2 mx-4 mt-2 rounded-2xl">
-            {chat.name}
-          </div>
-        ))}
-      </aside>
-    );
-  }
+export default function ChatsList({Chats}) {
+  const [chats] = useState(Chats);
+  return (
+    <aside className="bg-primary border-r-1 border-secondary gap-2 flex h-full absolute w-64 flex-col">
+      {chats.map((chat, index) => (
+        <div key={index} className="bg-secondary hover:bg-tertiary p-2 mx-4 mt-2 rounded-2xl">
+          {chat.name}
+        </div>
+      ))}
+    </aside>
+  );
 }
