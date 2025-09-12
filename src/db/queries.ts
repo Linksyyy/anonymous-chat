00d173808847f4d7f6525430ee0343bbdfd53e5a
Linksyyy <enzovidero@gmail.com>
2025-09-12 07:27:43 -0300

    Added salt generation for end-to-end encryption and maintenance in the user table

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,15 +1,20 @@
 import { eq } from "drizzle-orm";
 import { db } from "./db";
 import { users } from "./schemas";
+import { randomBytes } from "crypto";
 
 export async function findUser(username: string) {
-  const result = await db.select().from(users).where(eq(users.username, username));
-  return result[0]
+  const result = await db
+    .select()
+    .from(users)
+    .where(eq(users.username, username));
+  return result[0];
 }
 
 export async function registerUser(username: string, password_hash: string) {
   try {
-    await db.insert(users).values({ username, password_hash });
+    const ee_salt = randomBytes(16).toHex();
+    await db.insert(users).values({ username, password_hash, ee_salt });
   } catch (error) {
     throw new Error(error);
   }
--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -2,6 +2,7 @@ import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid().unique().primaryKey().defaultRandom(),
-  username: varchar({ length: 255 }).unique(),
-  password_hash: varchar({ length: 255 }),
+  username: varchar({ length: 255 }).unique().notNull(),
+  password_hash: varchar({ length: 255 }).notNull(),
+  ee_salt: varchar({ length: 255 }).unique().notNull(),
 });
