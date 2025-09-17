8d406288c8834db84939a42708a136d515e5916c
Linksyyy <enzovidero@gmail.com>
2025-09-17 11:40:48 -0300

    Removes `findChatByParticipants` query and simplify `createParticipant` params

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,4 +1,4 @@
-import { eq, sql } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { db } from "./db";
 import { chats, participants, users } from "./schemas";
 import { randomBytes } from "crypto";
@@ -17,13 +17,13 @@ export async function registerUser(username: string, password_hash: string) {
 }
 
 export async function createParticipant(
-  userId: string,
-  chatId: string,
+  user_id: string,
+  chat_id: string,
   role: "guest" | "admin" = "guest"
 ) {
   return await db
     .insert(participants)
-    .values({ user_id: userId, chat_id: chatId, role })
+    .values({ user_id, chat_id, role })
     .returning();
 }
 
@@ -51,25 +51,4 @@ export async function findParticipationsOfUser(userId: string) {
     },
   });
   return participations;
-}
-
-export async function findChatByParticipants(user1Id: string, user2Id: string) {
-  const result = await db
-    .select({ chatId: participants.chat_id })
-    .from(participants)
-    .where(
-      sql`chat_id IN (SELECT p1.chat_id FROM ${participants} p1 JOIN ${participants} p2 ON p1.chat_id = p2.chat_id WHERE p1.user_id = ${user1Id} AND p2.user_id = ${user2Id})`
-    )
-    .groupBy(participants.chat_id)
-    .having(sql`count(${participants.user_id}) = 2`);
-
-  if (result.length === 0) {
-    return [];
-  }
-
-  const chatDetails = await db.query.chats.findFirst({
-    where: eq(chats.id, result[0].chatId),
-  });
-
-  return chatDetails ? [chatDetails] : [];
 }
