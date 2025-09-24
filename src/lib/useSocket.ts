53f402ad69017adb24ede8db63986990a5a9b42c
Linksyyy <enzovidero@gmail.com>
2025-09-24 17:50:42 -0300

    Fixes cleanup of socket listeners

--- src/lib/useSocket.ts
+++ src/lib/useSocket.ts
@@ -4,6 +4,8 @@ import { socket } from "./socket";
 export function useSocket(event, callback) {
   useEffect(() => {
     socket.on(event, callback);
-    return () => socket.off(event, callback);
+    return () => {
+      socket.off(event, callback);
+    };
   }, [event, callback]);
 }
