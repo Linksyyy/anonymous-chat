e2a656cae8e02fa0e5b839a856115fa5595c931a
Linksyyy <enzovidero@gmail.com>
2025-09-29 11:30:46 -0300

    Improves key status display logic

--- src/Components/KeyStatus.jsx
+++ src/Components/KeyStatus.jsx
@@ -28,20 +28,30 @@ export default function KeyStatus({ toggleVisible }) {
         <div className="p-4 border-b border-primary-2">
           <h2 className="text-lg font-semibold">Keys Manager</h2>
         </div>
-        <div>
-          {actualUserManager.chats.map((chat) => (
-            <div
-              key={chat.id}
-              className="px-6 flex justify-between border-primary-2 border-t-2"
-            >
-              <h2 className="text-xl h-10">{chat.title}</h2>
-              {keys?.get(chat.id) ? (
-                <FiCheck className="size-8 text-green-600" />
-              ) : (
-                <FiX className="size-8 text-red-800" />
-              )}
+        <div className="px-4">
+          {!actualUserManager.chats === 0 ? (
+            <div className="flex justify-center items-center">
+              <p className="text-gray-400">
+                You aren't into any chat to have keys
+              </p>
             </div>
-          ))}
+          ) : (
+            <ul>
+              {actualUserManager.chats.map((chat) => (
+                <li
+                  key={chat.id}
+                  className="px-6 flex py-2 justify-between border-primary-2 not-last:border-b-1"
+                >
+                  <h2 className="text-xl h-10 trucante flex items-center">{chat.title}</h2>
+                  {keys?.get(chat.id) ? (
+                    <FiCheck className="size-8 text-green-600" />
+                  ) : (
+                    <FiX className="size-8 text-red-800" />
+                  )}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
